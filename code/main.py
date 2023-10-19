9347fb2c0db0420269ae3d17fc0b0effea2b06b4
ABleasdale1 <112654381+ABleasdale1@users.noreply.github.com>
2023-10-19 23:29:58 +0100

    Finally got / commands working

--- code/main.py
+++ code/main.py
@@ -1,52 +1,79 @@
 import discord
+from discord.ext import commands
+from discord import app_commands
 import asyncio
 
+class Bot(commands.Bot):
+    def __init__(self):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        help_command=None
+        super().__init__(command_prefix = "?", intents = intents)
+        
 
-intents = discord.Intents.default()
-intents.message_content = True
+    async def setup_hook(self):
+        await self.tree.sync(guild = discord.Object(id = 1156650436611289239))
+        print(f"Synced slash commands for {self.user}.")
+    
+    async def on_command_error(self, ctx, error):
+        await ctx.reply(error, ephemeral = True)
 
-client = discord.Client(intents=intents)
+bot = Bot()
+
+@bot.hybrid_command(name = "test", with_app_command = True, description = "Testing")
+@app_commands.guilds(discord.Object(id = 1156650436611289239))
+@commands.has_permissions(administrator = True)
+async def test(ctx: commands.Context):
+    await ctx.defer(ephemeral = True)
+    await ctx.reply("hi!")
 
 async def update_status():
     while True:
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you"))
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="you"))
         await asyncio.sleep(5)
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="to your mom moan my name"))
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="to your mom moan my name"))
         await asyncio.sleep(5)
-        await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="with your mom"))
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="with your mom"))
         await asyncio.sleep(5)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
-    client.loop.create_task(update_status())
+    print(f'We have logged in as {bot.user}')
+    bot.loop.create_task(update_status())
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('<help'):
-        await message.channel.send('The commands available are `Hello`, `Boo`, `Ping`')
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Go fuck yourself!')
 
-    if message.content.startswith('<hello'):
-        await message.channel.send('Go fuck yourself!')
+@bot.command()
+async def commands(ctx):
+    await ctx.send('The commands available are `hello`, `boo`, `ping`, `hello there`')
 
-    if message.content.startswith('<boo'):
-        await message.channel.send('Your mom is a fucking whore!')
+@bot.command()
+async def ping(ctx):
+    if ctx.channel.id == 1156650437005561878:
+            await ctx.send(f"Sorry {ctx.author.mention} I can't do that here")
+    else:
+        counter = 0
+        await ctx.send('Why have you done this')
+        while counter <= 15:
+            await ctx.send(f'{ctx.author.mention} Fuck off!')
+            await asyncio.sleep(1)
+            counter = counter + 1
 
-    if message.content.startswith('<ping'):
-        if message.channel.id == 1156650437005561878:
-            await message.channel.send(f"Sorry {message.author.mention} I can't do that here")
-        else:
-            counter = 0
-            await message.channel.send('Why have you done this')
-            while counter <= 15:
-                await message.channel.send(f'{message.author.mention} Pong!')
-                await asyncio.sleep(1)
-                counter = counter + 1
+@bot.hybrid_command()
+async def testing(ctx):
+    await ctx.send("This is a hybrid command!")
+
+@bot.hybrid_group(fallback="get")
+async def tag(ctx, name):
+    await ctx.send(f"Showing tag: {name}")
+
+@tag.command()
+async def create(ctx, name):
+    await ctx.send(f"Created tag: {name}")
 
 with open('fuckYou/token.txt', 'r') as file:
     token = file.read().replace('\n', '')
 
-client.run(token)
+bot.run(token)
